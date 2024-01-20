b2e9573b83e8bc2df4f33b5ebffdd5adad47c510
Muhammad Fadlil Ismail <67428554+mfadlili@users.noreply.github.com>
2024-01-20 14:12:48 +0000

    add new line in docker file and hello.py

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,7 @@ FROM python:3.9.1
 
 RUN pip install pandas
 
+WORKDIR /app
+COPY hello.py hello.py 
+
 ENTRYPOINT [ "bash" ]
---
+++ hello.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df = pd.DataFrame(
+    {'id': [1,2,3],
+     'nama': ['bagas', 'aji', 'riyad']}
+)
+
+print(df)
